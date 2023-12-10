ad3719d381519ad0e9783ab20c571cb3f8b2a3e1
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-12-09 19:05:24 -0600

    made saga function and get request for current movie genres

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,38 +1,64 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga';
-import { takeEvery, put } from 'redux-saga/effects';
-import axios from 'axios';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import { takeEvery, put, takeLatest } from "redux-saga/effects";
+import axios from "axios";
 
 // Create the rootSaga generator function
 function* rootSaga() {
-  yield takeEvery('FETCH_MOVIES', fetchAllMovies);
+  yield takeEvery("SAGA/FETCH_MOVIES", fetchAllMovies);
+  yield takeLatest("SAGA/SET_CURRENT_MOVIE", getCurrentMovieGenres);
 }
 
 function* fetchAllMovies() {
   try {
     // Get the movies:
-    const moviesResponse = yield axios.get('/api/movies');
+    const moviesResponse = yield axios.get("/api/movies");
     // Set the value of the movies reducer:
     yield put({
-      type: 'SET_MOVIES',
-      payload: moviesResponse.data
+      type: "SET_MOVIES",
+      payload: moviesResponse.data,
     });
   } catch (error) {
-    console.log('fetchAllMovies error:', error);
+    console.log("fetchAllMovies error:", error);
+  }
+}
+
+function* getCurrentMovieGenres(action) {
+  try {
+    const movieResponse =
+      yield axios.get(`/api/genres/${action.payload.id}`);
+      // yield axios({
+      //   method: "GET",
+      //   url: `/api/genres/${action.payload.id}`,
+      // });
+    console.log("movieResponse.data: ", movieResponse.data);
+    yield put({
+      type: "SET_CURRENT_MOVIE_GENRES",
+      payload: movieResponse.data,
+    });
+  } catch (error) {
+    console.log("Error getting movie genres: ", error);
   }
 }
 
 // Set current movie:
-const currentMovie = (state={}, action) => {
+const currentMovie = (state = {}, action) => {
   switch (action.type) {
-    case 'SET_CURRENT_MOVIE':
+    case "SAGA/SET_CURRENT_MOVIE":
       return action.payload;
-      case 'RESET_CURRENT_MOVIE':
-        return {}
+    case "RESET_CURRENT_MOVIE":
+      return {};
   }
   return state;
-}
+};
+
+// Set current movie genres:
+const currentMovieGenres = (state = [], action) => {
+  if (action.type === "SET_CURRENT_MOVIE_GENRES") {
+    return action.payload;
+  } else return state;
+};
 
 // Create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
@@ -40,32 +66,33 @@ const sagaMiddleware = createSagaMiddleware();
 // Used to store movies returned from the server
 const movies = (state = [], action) => {
   switch (action.type) {
-    case 'SET_MOVIES':
+    case "SET_MOVIES":
       return action.payload;
     default:
       return state;
   }
-}
+};
 
 // Used to store the movie genres
 const genres = (state = [], action) => {
   switch (action.type) {
-    case 'SET_GENRES':
+    case "SET_GENRES":
       return action.payload;
     default:
       return state;
   }
-}
+};
 
 // Create one store that all components can use
 const storeInstance = createStore(
   combineReducers({
     movies,
     genres,
-    currentMovie
+    currentMovie,
+    currentMovieGenres,
   }),
   // Add sagaMiddleware to our store
-  applyMiddleware(sagaMiddleware, logger),
+  applyMiddleware(sagaMiddleware, logger)
 );
 
 // Pass rootSaga into our sagaMiddleware
