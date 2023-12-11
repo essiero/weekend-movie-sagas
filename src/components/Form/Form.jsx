d7ee3dea1403a861ab2db8a2295040e86ab23cf9
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-12-11 00:57:14 -0600

    made function to create object from new movie input fields and send a dispatch to store

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,30 +1,40 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useState } from 'react';
+import { useHistory, HashRouter as Router, Route, Link } from "react-router-dom";
 
 function Form() {
     const dispatch = useDispatch();
+    const history = useHistory();
     const [newMovieObject, setNewMovieObject] = useState({})
+    // const formShown = useSelector((store) => store.formShown)
 
+    // POST dispatch with newMovieObject to send to store
     const postNewMovie = () => {
+        console.log("newMovieObject: ", newMovieObject)
         dispatch({
-            type: 'POST',
-            url: '/api/movies',
-            data: newMovieObject
-        })
+            type: 'SAGA/ADD_MOVIE',
+            payload: newMovieObject });
+// then return us to the main page
+       history.push("/")
     }
-    const setGenre = (value) => {
-        setNewMovieObject(value)
+    // function to make object with a given key and corresponding value
+    // and then store it in React state "newMovieObject"
+    const handleNameChange = (input, event) => {
+        event.preventDefault();
+        setNewMovieObject({...newMovieObject,
+        [input]: event.target.value
+    })
     }
 return (
     <div id="formDiv">
     <form align="center">
-      <input placeholder="Title"></input>
-      <input placeholder='Movie poster URL'></input>
-      <input placeholder='Description'></input>
-      <select onChange={setGenre}>
-        <option value="" disabled selected>Select genre</option>
+      <input placeholder="Title" onChange={() => handleNameChange("title", event)}></input>
+      <input placeholder='Movie poster URL' onChange={() => handleNameChange("poster", event)}></input>
+      <input placeholder='Description' onChange={() => handleNameChange("description", event)}></input>
+      <select onChange={() => handleNameChange("genre_id", event)}>
+        <option value="" disabled>Select genre</option>
         <option value="1">Adventure</option>
         <option value="2">Animated</option>
         <option value="3">Biographical</option>
@@ -40,16 +50,18 @@ return (
         <option value="13">Superhero</option>
       </select>
       <br />
+      <Router>
+        <Link to="/">
       <Button variant="outlined"
       sx={{
         margin: 1
       }} 
-      onClick={history.push('/')}
       >Cancel</Button>
+      </Link>
       <Button variant="contained"
       onClick={postNewMovie}
       >Save</Button>
-
+</Router>
       </form>
       </div>
 )
