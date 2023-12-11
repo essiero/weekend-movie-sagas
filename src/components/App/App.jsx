9dbb6610a30824c9d6c41b6fc29358c4920dc7f4
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-12-10 18:12:07 -0600

    installed material ui

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,11 +2,20 @@ import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList';
 import MovieDetails from '../MovieDetails.jsx/MovieDetails';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { useHistory } from 'react-router-dom';
 
 function App() {
+const history = useHistory();
+
+  const addNewMovie = () => {
+    console.log("here is am, the button is clicking")
+  }
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
+      <Button variant="contained" onClick={addNewMovie}>Add movie</Button>
       <Router>        
         <Route path="/" exact>
           <MovieList />
